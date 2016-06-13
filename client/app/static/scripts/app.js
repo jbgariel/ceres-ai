41ab60f89b9937caec7748663ac66ba0a636d943
Jibsgrl <jbgariel@gmail.com>
2016-06-13 18:54:30 +0200

    first commi

--- client/app/static/scripts/app.js
+++ client/app/static/scripts/app.js
@@ -1,35 +1,152 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name clientApp
- * @description
- * # clientApp
- *
- * Main module of the application.
- */
-angular
-  .module('clientApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngSanitize',
-    'ngTouch',
-    'ui.router',
-    'restangular'
+ angular
+ /*.config(config)*/
+ /*.run(run)*/
+ .module('app', [
+  'ngAnimate',
+   /*'ngRoute',*/
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngSanitize',
+  'ngTouch',
+  'ui.router',
+  'restangular'
   ])
-  .config(function ($locationProvider, $urlRouterProvider, $stateProvider, $httpProvider) {
-    $locationProvider.html5Mode(true);
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
 
-    $urlRouterProvider.otherwise('/');
+ .config(function ($locationProvider, $urlRouterProvider, $stateProvider, $httpProvider) {
+  $locationProvider.html5Mode(true);
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
 
-    $stateProvider
-      .state('home', {
-        url: '/',
-        controller: 'MainCtrl',
-        templateUrl: 'static/views/main.html'
-      });
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+  .state('main', {
+    url: '/',
+    templateUrl: 'static/views/main.html',
+    controller: 'MainCtrl',
+  })
+
+  .state('suscribe', {
+    url: "/suscribe",
+    templateUrl: "static/views/suscribe.html",
+    controller: 'RegisterController'
+  })
+
+  .state('login', {
+    url: "/login",
+    templateUrl: "static/views/login.html",
+    controller: 'LoginController'
+  })
+
+  .state('home', {
+    url: "/home",
+    templateUrl: "static/views/home.html",
+    controller: 'HomeController'
   });
+
+});
+
+ run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+ function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+          }
+
+          $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+              $location.path('/login');
+            }
+          });
+        }
+
+
+
+/*
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute',
+          'ngAnimate',
+          'ngRoute',
+          'ngAria',
+          'ngCookies',
+          'ngMessages',
+          'ngSanitize',
+          'ngTouch',
+          'ui.router',
+          'restangular'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider', '$locationProvider', '$urlRouterProvider', '$stateProvider', '$httpProvider'];
+    function config($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        
+        $routeProvider
+            .when('/', {
+                controller: 'MainCtrl',
+                templateUrl: 'static/views/main.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/home', {
+                controller: 'HomeController',
+                templateUrl: 'static/views/home.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'static/views/login.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/suscribe', {
+                controller: 'RegisterController',
+                templateUrl: 'static/views/suscribe.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/' });
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
+
+})();
+
+*/
+
+
+
+
+
+
--- client/app/static/scripts/controllers/main.js
+++ client/app/static/scripts/controllers/main.js
@@ -5,13 +5,23 @@
  * @name clientApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the clientApp
+ * Controller of the app
  */
-angular.module('clientApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'Create Django models',
-      'Expose them through a REST API',
-      'Consume them with restangular'
-    ];
+
+var app = angular.module('app');
+
+app.controller('MainCtrl', function ($scope) {
+    //$scope.todos = [
+    //  'Create Django models',
+    //  'Expose them through a REST API',
+    //  'Consume them with restangular'
+    //];
+    $scope.suscribe = function(){
+  		$location.url('/suscribe.html');
+	};
+
+	$scope.login = function(){
+  		$location.url('/login.html');
+	};
+
   });
